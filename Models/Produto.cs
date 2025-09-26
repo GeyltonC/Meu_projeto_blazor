5084f665e5d076a3199bbb42fc836942edaae3f9
Geylton Candido <91790501+GeyltonC@users.noreply.github.com>
2025-09-26 08:59:35 -0300

    Aula do dia 26/09/2025

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -5,7 +5,7 @@
         public string Nome { get; set; }
         public int Quantidade { get; set; }
         public double Preco { get; set; }
-
+        
         public double ValorEstoqueTotal()
         {
             return Preco * Quantidade;
